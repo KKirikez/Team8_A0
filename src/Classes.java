7b2bec558af48222299d358742ee136231bedac4
kkiri252 <kostyakirikeza@gmail.com>
2024-01-18 20:54:53 -0700

    added classes and class skeletons

---
+++ src/Classes.java
@@ -0,0 +1,42 @@
+
+public class Classes {
+	public static String[] readFile() {
+//		Reads file and returns a string
+		String[] data = {""};
+		return data;
+	}
+	
+	public static void writeFile(String[] data) {
+//		Writes files from an array
+	}
+	
+	public static int languagePoints() {
+//		Calculates and returns language points
+		return 0;
+	}
+	
+	public static int educationPoints() {
+//		Calculates language points
+		return 0;
+	}
+	
+	public static int workPoints() {
+//		Calculates language points
+		return 0;
+	}
+	
+	public static int agePoints() {
+//		Calculates language points
+		return 0;
+	}
+	
+	public static int arrangedWorkPoints() {
+//		Calculates language points
+		return 0;
+	}
+	
+	public static int adaptabilityPoints() {
+//		Calculates language points
+		return 0;
+	}
+}
