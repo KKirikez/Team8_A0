721ad7d36f30d1b89675c5cd3ff2984ebe2e13b4
kkiri252 <kostyakirikeza@gmail.com>
2024-01-22 18:07:46 -0700

    Added input and output folders

--- src/AppDriver.java
+++ src/AppDriver.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
 public class AppDriver {
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException{
+		Classes c = new Classes();
 		
+		String[] data = {"a","s","d"};
+		c.writeFile(data);
+		
+		System.out.println(c.readFile());
 	}
 }
